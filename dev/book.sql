3732a6e27f15e4f6162bc897c75e82ce5888f6a4
radekmalysa <rm@pawelec.info>
2026-09-24 16:35:13 +0200

    xxxy

--- dev/book.sql
+++ dev/book.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 17, 2026 at 03:12 PM
+-- Generation Time: Sep 24, 2026 at 02:34 PM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.30
 
@@ -55,9 +55,11 @@ CREATE TABLE `book_appointments` (
 --
 
 INSERT INTO `book_appointments` (`id`, `service_id`, `customer_id`, `salon_id`, `visit_time`, `admission`, `guide`, `cinema`, `kulturalna_szkola`, `kultura_za_zl`, `pax`, `state`, `total_price`, `sell_price`, `sell_doc`, `notes`, `create_time`, `create_ip`, `update_time`, `update_ip`) VALUES
-(1, 1, 1, 1, '2026-09-15 09:00:00', 0, 0, 0, 0, 0, 0, 'confirmed', 100.00, 0.00, '', '', '2026-09-06 19:59:14', '', '2026-09-06 19:59:14', '127.0.0.1'),
-(2, 3, 2, 1, '2026-09-16 10:00:00', 1, 0, 1, 1, 1, 25, 'pending', 100.00, 0.00, '', 'ta rezerwacja jest moja', '2026-09-06 19:59:14', '', '2026-09-06 19:59:14', '127.0.0.1'),
-(3, 2, 5, 1, '2026-09-18 08:00:00', 0, 0, 0, 0, 0, 20, 'pending', 0.00, 0.00, '', 'test nr 2', '2026-09-17 16:05:18', '127.0.0.1', '2026-09-17 16:05:18', '127.0.0.1');
+(1, 1, 1, 1, '2026-09-22 09:00:00', 0, 0, 0, 0, 0, 0, 'confirmed', 100.00, 0.00, '', '', '2026-09-06 19:59:14', '', '2026-09-06 19:59:14', '127.0.0.1'),
+(2, 3, 2, 1, '2026-09-23 10:00:00', 1, 0, 1, 1, 1, 25, 'pending', 100.00, 0.00, '', 'ta rezerwacja jest moja', '2026-09-06 19:59:14', '', '2026-09-06 19:59:14', '127.0.0.1'),
+(3, 2, 5, 1, '2026-09-25 08:00:00', 0, 0, 0, 0, 0, 20, 'pending', 0.00, 0.00, '', 'test nr 2', '2026-09-17 16:05:18', '127.0.0.1', '2026-09-17 16:05:18', '127.0.0.1'),
+(4, 3, 6, 1, '2026-09-25 11:00:00', 0, 0, 1, 0, 0, 20, 'confirmed', 0.00, 0.00, '', '', '2026-09-22 09:15:18', '127.0.0.1', '2026-09-22 09:15:18', '127.0.0.1'),
+(5, 1, 7, 1, '2026-09-25 09:00:00', 0, 0, 0, 0, 0, 0, 'confirmed', 0.00, 0.00, '', 'Sprzątanie sali', '2026-09-22 09:17:45', '127.0.0.1', '2026-09-22 09:17:45', '127.0.0.1');
 
 -- --------------------------------------------------------
 
@@ -68,6 +70,7 @@ INSERT INTO `book_appointments` (`id`, `service_id`, `customer_id`, `salon_id`, 
 CREATE TABLE `book_appointment_providers` (
   `id` int UNSIGNED NOT NULL,
   `appointment_id` int NOT NULL,
+  `service_id` smallint UNSIGNED NOT NULL DEFAULT '0',
   `provider_id` int UNSIGNED NOT NULL DEFAULT '0',
   `start_time` datetime DEFAULT NULL,
   `end_time` datetime DEFAULT NULL
@@ -77,10 +80,12 @@ CREATE TABLE `book_appointment_providers` (
 -- Dumping data for table `book_appointment_providers`
 --
 
-INSERT INTO `book_appointment_providers` (`id`, `appointment_id`, `provider_id`, `start_time`, `end_time`) VALUES
-(1, 1, 1, '2026-09-15 09:00:00', '2026-09-16 16:00:00'),
-(2, 2, 2, '2026-09-16 10:00:00', '2026-09-16 11:45:00'),
-(3, 3, 1, '2026-09-18 08:00:00', '2026-09-18 10:30:00');
+INSERT INTO `book_appointment_providers` (`id`, `appointment_id`, `service_id`, `provider_id`, `start_time`, `end_time`) VALUES
+(1, 1, 0, 1, '2026-09-22 09:00:00', '2026-09-23 16:00:00'),
+(2, 2, 0, 2, '2026-09-23 10:00:00', '2026-09-23 11:45:00'),
+(3, 3, 2, 1, '2026-09-25 08:00:00', '2026-09-25 10:30:00'),
+(4, 4, 0, 1, '2026-09-25 11:00:00', '2026-09-25 12:45:00'),
+(5, 5, 0, 2, '2026-09-25 09:00:00', '2026-09-25 11:00:00');
 
 -- --------------------------------------------------------
 
@@ -125,7 +130,14 @@ CREATE TABLE `book_categories_services` (
 --
 
 INSERT INTO `book_categories_services` (`category_id`, `service_id`) VALUES
-(1, 1);
+(1, 1),
+(1, 5),
+(1, 6),
+(1, 7),
+(2, 7),
+(2, 8),
+(2, 9),
+(2, 10);
 
 -- --------------------------------------------------------
 
@@ -156,7 +168,9 @@ CREATE TABLE `book_customers` (
 INSERT INTO `book_customers` (`id`, `customer_type`, `name`, `address`, `email`, `phone`, `contact_name`, `contact_phone`, `contact_email`, `pax_care`, `accept_processing`, `accept_regulations`, `accept_kultura_zl`) VALUES
 (1, NULL, 'Elektrownia', '', '', '', '', '', '', 0, 0, 0, 0),
 (2, 'primary', 'Szkoła Podstawowa nr 30', 'Graniczna 17/9', 'radek.malysa@gmail.com', '222', 'Joanna Kowalska', '997', 'radek.malysa@gmail.com', 1, 1, 0, 0),
-(5, NULL, '', '', '', '', '', '', '', 0, 0, 0, 0);
+(5, 'post_primary', 'Liceum Ogólnokształcące', '', '', '', '', '', '', 0, 0, 0, 0),
+(6, 'primary', 'Szkoła Testowa', '', '', '', '', '', '', 0, 0, 0, 0),
+(7, 'other', 'Radek', '', '', '', '', '', '', 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -168,18 +182,22 @@ CREATE TABLE `book_providers` (
   `id` int NOT NULL,
   `salon_id` int NOT NULL,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL,
-  `last_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL,
-  `description` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL,
-  `state` tinyint(1) DEFAULT '1'
+  `last_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL DEFAULT '',
+  `description` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL DEFAULT '',
+  `state` tinyint(1) DEFAULT '1',
+  `ord` tinyint UNSIGNED NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
 --
 -- Dumping data for table `book_providers`
 --
 
-INSERT INTO `book_providers` (`id`, `salon_id`, `name`, `last_name`, `description`, `state`) VALUES
-(1, 1, 'Sala edukacyjna 1', '', '', 1),
-(2, 1, 'Sala wystawowa', '', '', 1);
+INSERT INTO `book_providers` (`id`, `salon_id`, `name`, `last_name`, `description`, `state`, `ord`) VALUES
+(1, 1, 'Sala wystawowa 1 piętro', '', '', 1, 1),
+(2, 1, 'Sala wystawowa parter', '', '', 1, 2),
+(3, 1, 'Sala edukacyjna', '', '', 1, 4),
+(4, 1, 'Piec', '', '', 1, 3),
+(5, 1, 'Biblioteka', '', '', 1, 5);
 
 -- --------------------------------------------------------
 
@@ -193,6 +211,17 @@ CREATE TABLE `book_providers_services` (
   `custom_price` decimal(10,2) DEFAULT NULL,
   `custom_duration` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `book_providers_services`
+--
+
+INSERT INTO `book_providers_services` (`provider_id`, `service_id`, `custom_price`, `custom_duration`) VALUES
+(1, 0, NULL, NULL),
+(1, 8, NULL, NULL),
+(1, 9, NULL, NULL),
+(2, 6, NULL, NULL),
+(2, 9, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -224,11 +253,14 @@ INSERT INTO `book_salons` (`id`, `name`, `address`, `phone`, `timezone`, `create
 
 CREATE TABLE `book_services` (
   `id` int NOT NULL,
+  `service_type` enum('lesson','tour','cinema','blockade') COLLATE utf8mb4_polish_ci NOT NULL DEFAULT 'lesson',
   `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL,
   `description` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL DEFAULT '',
   `price` decimal(10,2) DEFAULT NULL,
   `duration` int DEFAULT NULL,
   `online` tinyint UNSIGNED NOT NULL DEFAULT '0',
+  `pax_min` tinyint UNSIGNED NOT NULL DEFAULT '0',
+  `pax_max` tinyint UNSIGNED NOT NULL DEFAULT '0',
   `state` tinyint UNSIGNED NOT NULL DEFAULT '0',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `create_ip` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci NOT NULL DEFAULT '',
@@ -240,14 +272,19 @@ CREATE TABLE `book_services` (
 -- Dumping data for table `book_services`
 --
 
-INSERT INTO `book_services` (`id`, `name`, `description`, `price`, `duration`, `online`, `state`, `create_time`, `create_ip`, `update_time`, `update_ip`) VALUES
-(1, 'Rezerwacja sali', 'Blokowanie sali dla celów organizacyjnych', 100.00, NULL, 0, 1, '2026-09-06 18:00:28', '', '2026-09-06 18:00:28', '127.0.0.1'),
-(2, 'Spacer po wystawie i warsztaty plastyczne', '', NULL, NULL, 0, 1, '2026-09-09 13:52:40', '127.0.0.1', '2026-09-09 13:52:40', '127.0.0.1'),
-(3, 'Nowe Horyzonty Edukacji Filmowej', '', NULL, NULL, 0, 1, '2026-09-09 14:28:39', '127.0.0.1', '2026-09-09 14:28:39', '127.0.0.1'),
-(4, 'Warsztaty ruchowo-plastyczne podczas spaceru po wystawie', '', 0.00, 0, 0, 1, '2026-09-09 14:28:52', '127.0.0.1', '2026-09-09 14:28:52', '127.0.0.1'),
-(5, 'Seanse filmowe z bieżącego repertuaru', '', 100.00, 60, 0, 1, '2026-09-09 14:29:06', '127.0.0.1', '2026-09-09 14:29:06', '127.0.0.1'),
-(6, 'Warsztaty sensoryczno-plastyczne „Bajki z pieca”', '', NULL, NULL, 0, 1, '2026-09-09 14:29:22', '127.0.0.1', '2026-09-09 14:29:22', '127.0.0.1'),
-(7, 'Filmowe pokazy specjalne', '', 125.00, 60, 1, 1, '2026-09-09 14:29:33', '127.0.0.1', '2026-09-09 14:29:33', '127.0.0.1');
+INSERT INTO `book_services` (`id`, `service_type`, `name`, `description`, `price`, `duration`, `online`, `pax_min`, `pax_max`, `state`, `create_time`, `create_ip`, `update_time`, `update_ip`) VALUES
+(1, 'blockade', 'Rezerwacja sali', 'Blokowanie sali dla celów organizacyjnych', 100.00, NULL, 0, 0, 0, 1, '2026-09-06 18:00:28', '', '2026-09-06 18:00:28', '127.0.0.1'),
+(2, 'lesson', 'Spacer po wystawie i warsztaty plastyczne', '', NULL, NULL, 0, 0, 0, 1, '2026-09-09 13:52:40', '127.0.0.1', '2026-09-09 13:52:40', '127.0.0.1'),
+(3, 'lesson', 'Nowe Horyzonty Edukacji Filmowej', '', NULL, NULL, 0, 0, 0, 1, '2026-09-09 14:28:39', '127.0.0.1', '2026-09-09 14:28:39', '127.0.0.1'),
+(4, 'lesson', 'Warsztaty ruchowo-plastyczne podczas spaceru po wystawie', '', 0.00, 0, 0, 0, 0, 1, '2026-09-09 14:28:52', '127.0.0.1', '2026-09-09 14:28:52', '127.0.0.1'),
+(5, 'lesson', 'Seanse filmowe z bieżącego repertuaru', '', 100.00, 60, 0, 0, 0, 1, '2026-09-09 14:29:06', '127.0.0.1', '2026-09-09 14:29:06', '127.0.0.1'),
+(6, 'lesson', 'Warsztaty sensoryczno-plastyczne „Bajki z pieca”', '', 0.00, 0, 1, 0, 0, 1, '2026-09-09 14:29:22', '127.0.0.1', '2026-09-09 14:29:22', '127.0.0.1'),
+(7, 'lesson', 'Filmowe pokazy specjalne', '', 125.00, 60, 1, 0, 0, 1, '2026-09-09 14:29:33', '127.0.0.1', '2026-09-09 14:29:33', '127.0.0.1'),
+(8, 'lesson', 'Warsztaty plastyczne w sali edukacyjnej (cykl 5 spotkań)', '', 20.00, 60, 0, 0, 0, 1, '2026-09-22 09:28:15', '127.0.0.1', '2026-09-22 09:28:15', '127.0.0.1'),
+(9, 'lesson', 'Seanse filmowe z bieżącego repertuaru', '', 16.00, 0, 0, 0, 0, 1, '2026-09-22 09:30:11', '127.0.0.1', '2026-09-22 09:30:11', '127.0.0.1'),
+(10, 'lesson', 'Warsztaty plastyczne oraz spacer po wystawie', '', 20.00, 70, 0, 10, 25, 1, '2026-09-22 09:38:27', '127.0.0.1', '2026-09-22 09:38:27', '127.0.0.1'),
+(11, 'tour', 'Zwiedzanie wystaw', 'Bez przewodnika', 0.00, 0, 0, 0, 0, 1, '2026-09-24 12:31:05', '', '2026-09-24 12:31:05', '127.0.0.1'),
+(12, 'tour', 'Zwiedzanie wystaw z przewodnikiem', '', 0.00, 0, 0, 0, 0, 1, '2026-09-24 12:31:05', '', '2026-09-24 12:31:05', '127.0.0.1');
 
 -- --------------------------------------------------------
 
@@ -437,13 +474,13 @@ ALTER TABLE `book_working_hours`
 -- AUTO_INCREMENT for table `book_appointments`
 --
 ALTER TABLE `book_appointments`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `book_appointment_providers`
 --
 ALTER TABLE `book_appointment_providers`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `book_categories`
@@ -455,13 +492,13 @@ ALTER TABLE `book_categories`
 -- AUTO_INCREMENT for table `book_customers`
 --
 ALTER TABLE `book_customers`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `book_providers`
 --
 ALTER TABLE `book_providers`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `book_salons`
@@ -473,7 +510,7 @@ ALTER TABLE `book_salons`
 -- AUTO_INCREMENT for table `book_services`
 --
 ALTER TABLE `book_services`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `book_service_schedule`
